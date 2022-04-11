ca27259c208fea1b0e2c0d0055f23ab5568b9c24
johnj974 <jjdug@hotmail.com>
2022-04-11 14:52:07 +0100

    contactAndGalleryHomeSectionsStyled+Connected

--- src/app/home/home-contact/home-contact.component.ts
+++ src/app/home/home-contact/home-contact.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-contact',
   templateUrl: './home-contact.component.html',
-  styleUrls: ['./home-contact.component.css']
+  styleUrls: ['./home-contact.component.css'],
 })
 export class HomeContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  //.
+  toContact() {
+    this.router.navigate(['/contact']);
   }
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
 }
--- src/app/home/home-gallery/home-gallery.component.ts
+++ src/app/home/home-gallery/home-gallery.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-gallery',
   templateUrl: './home-gallery.component.html',
-  styleUrls: ['./home-gallery.component.css']
+  styleUrls: ['./home-gallery.component.css'],
 })
 export class HomeGalleryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  //.
+  toGallery() {
+    this.router.navigate(['/gallery']);
   }
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
 }
