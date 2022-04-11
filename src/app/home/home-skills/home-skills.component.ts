1b3d30debc89b1548f7e53db682249b323caa610
johnj974 <jjdug@hotmail.com>
2022-04-11 13:36:10 +0100

    ServiceSectionAddedToHomePage

--- src/app/home/home-skills/home-skills.component.ts
+++ src/app/home/home-skills/home-skills.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { SkillsModel } from 'src/app/models/skills.model';
+import { SkillsService } from 'src/app/services/skills.service';
 
 @Component({
   selector: 'app-home-skills',
   templateUrl: './home-skills.component.html',
-  styleUrls: ['./home-skills.component.css']
+  styleUrls: ['./home-skills.component.css'],
 })
 export class HomeSkillsComponent implements OnInit {
-
-  constructor() { }
+  //.
+  serviceProvidedArray = [];
+  constructor(private skillsService: SkillsService) {}
 
   ngOnInit(): void {
+    this.serviceProvidedArray = this.skillsService.getSkillsArray();
   }
-
 }
--- src/app/services/skills.service.ts
+++ src/app/services/skills.service.ts
@@ -7,10 +7,26 @@ import { SkillsModel } from '../models/skills.model';
 export class SkillsService {
   //.
   skillsServiceArray: SkillsModel[] = [
-    new SkillsModel('Residential', 'Residential', 'Residential'),
-    new SkillsModel('Commercial', 'Commercial', 'Commercial'),
-    new SkillsModel('Decking', 'Decking', 'Decking'),
-    new SkillsModel('Interior', 'Interior', 'Interior'),
+    new SkillsModel(
+      'Residential',
+      'We offer an extensive range of services for residential projects which range from single room design to complete home redesigns.',
+      '../../assets/images/upstairs.jpg'
+    ),
+    new SkillsModel(
+      'Commercial',
+      'We provide an exceptional Building Maintenance service for many customers in retail, commercial and residential areas',
+      '../../assets/images/building.jpg'
+    ),
+    new SkillsModel(
+      'Decking',
+      'We specialize in creating high-performing, low maintenance, beautiful outdoor spaces for our customers.',
+      '../../assets/images/finish-deck.jpg'
+    ),
+    new SkillsModel(
+      'Interior',
+      'We offer an extensive interior carpentry service catering to everything from single interior design to complete interior refurbishment',
+      '../../assets/images/blue-stairs.jpg'
+    ),
   ];
 
   getSkillsArray() {
