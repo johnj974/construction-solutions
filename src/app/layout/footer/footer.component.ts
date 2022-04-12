63ff7d3bce99382bccb29127b3a95433fa889fde
johnj974 <jjdug@hotmail.com>
2022-04-12 12:06:04 +0100

    FooterStyle+FunctionalityAdded

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  //.
+  displayYear = new Date().getFullYear()
 
   constructor() { }
 
