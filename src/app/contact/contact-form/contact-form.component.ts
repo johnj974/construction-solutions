8532fbf1485374eaa6843b9ffae3bd862df72058
johnj974 <jjdug@hotmail.com>
2022-04-15 10:16:55 +0100

    ContactForm+ContactInfoAdded

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent implements OnInit {
+  //.
+  contactForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSubmit() {
+    console.log(this.contactForm);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl(null),
+      email: new FormControl(null),
+      phone: new FormControl(null),
+      subject: new FormControl(null),
+      message: new FormControl(null),
+    });
+  }
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -5,9 +5,10 @@ import { ContactRoutingModule } from './contact-routing.module';
 import { ContactMainComponent } from './contact-main/contact-main.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ContactMainComponent, ContactFormComponent],
-  imports: [CommonModule, ContactRoutingModule, SharedModule],
+  imports: [CommonModule, ContactRoutingModule, SharedModule,ReactiveFormsModule],
 })
 export class ContactModule {}
